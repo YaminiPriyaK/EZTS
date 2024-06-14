be1528acb0801fc6a7461f8f461e897c1a3aac21
YaminiPriyaK <138279273+YaminiPriyaK@users.noreply.github.com>
2024-06-14 09:51:29 +0530

    TemperatureConverter.py

--- TemperatureConverter.py
+++ TemperatureConverter.py
@@ -1,3 +1,14 @@
+""" Temperature Converter
+Objective: Create a class to convert temperatures between Celsius and Fahrenheit.
+
+Instructions:
+Define a class TemperatureConverter with static methods for converting temperatures.
+Implement a method to convert from Celsius to Fahrenheit and another method to convert from Fahrenheit to Celsius.
+Demonstrate using the methods to convert temperatures in both directions.
+
+Hint: Use the formulas F = C * 9/5 + 32 for Celsius to Fahrenheit conversion and C = (F - 32) * 5/9 for Fahrenheit to Celsius conversion."""
+
+
 class TemperatureConverter:
     @staticmethod
     def celsiusToFahrenheit(C):
